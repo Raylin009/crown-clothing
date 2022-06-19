80656144a792536d6fe147837191fd9a12e3d02e
raylin009 <raylin009@gmail.com>
2022-06-20 06:45:01 +0800

    Dynamically rendering chardcoded HTML elements

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
 const App = () =>{
+  
+  const categories = [
+    {
+      id: 1,
+      title: 'Hates',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ]
   return (
   <div className="categories-container">
-    <div className="category-container">
-      {/* <img /> */}
-      <div calssName="category-body-container">
-        <h2>Hats</h2>
+    {categories.map(({title, id}) => (
+      <div className="category-container">
+      <div className="background-image" />
+      <div calssName="category-body-container" id={id}>
+        <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
     </div>
-    <div className="category-container">
-      {/* <img /> */}
-      <div calssName="category-body-container">
-        <h2>Jackets</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>    <div className="category-container">
-      {/* <img /> */}
-      <div calssName="category-body-container">
-        <h2>Women</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>    <div className="category-container">
-      {/* <img /> */}
-      <div calssName="category-body-container">
-        <h2>Mans</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>    <div className="category-container">
-      {/* <img /> */}
-      <div calssName="category-body-container">
-        <h2>Hats</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>    <div className="category-container">
-      {/* <img /> */}
-      <div calssName="category-body-container">
-        <h2>Hats</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>
+    ))}
   </div>
   );
 }
