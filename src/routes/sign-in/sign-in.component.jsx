e0cae97baffe828b0312edbc5633fb8d79e99a8d
raylin009 <raylin009@gmail.com>
2022-06-23 09:51:13 +0800

    removed reidrect code

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,25 +1,10 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
-
 import { 
-  auth,
   signInWithGooglePopup,
-  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 
 const SignIn = () => {
-  useEffect(() => {
-    const handleRedir = async() => {
-      const  response = await getRedirectResult(auth);
-      if(response){
-        const userDocRef = await createUserDocumentFromAuth(response.user)
-      }
-    }
-    handleRedir()
-    .catch(console.error)
-  }, [])
 
   const logGoogleUser = async() => {
     const { user } = await signInWithGooglePopup();
@@ -31,9 +16,6 @@ const SignIn = () => {
       <h1>This is Sign In</h1>
       <button onClick={logGoogleUser}>
         Sign in with Google Popup
-      </button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign in with Google Redirect
       </button>
     </div>
   )
