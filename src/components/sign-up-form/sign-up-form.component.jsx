ead266c7ca686a61ac8ac94b564ce1406f907dac
raylin009 <raylin009@gmail.com>
2022-06-29 11:52:06 +0800

    refectored FormInput out of Sign-up-form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,11 @@
 import { useState } from 'react';
 
-import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import FormInput from '../form-input/form-input.component'
+
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth
+} from '../../utils/firebase/firebase.utils'
 
 const defaultFormFields = {
   displayName: '',
@@ -50,16 +55,16 @@ const SignUpForm = () => {
     <div>
       <h1>Sign up with your email and password</h1>
       <form onSubmit={handleSubmit}>
-        <label>Display</label>
-        <input
+        <FormInput
+          label="Display Name"
           type="text" 
           required onChange={handleChange} 
           name="displayName" 
           value={displayName}
         />
 
-        <label>Email</label>
-        <input 
+        <FormInput
+          label="Email" 
           type="email" 
           required 
           onChange={handleChange} 
@@ -67,8 +72,8 @@ const SignUpForm = () => {
           value={email}
         />
 
-        <label>Password</label>
-        <input 
+        <FormInput
+          label="Password" 
           type="password" 
           required 
           onChange={handleChange} 
@@ -76,8 +81,8 @@ const SignUpForm = () => {
           value={password}
         />
 
-        <label>Confirm Password</label>
-        <input 
+        <FormInput
+          label="Confirm Password" 
           type="password" 
           required 
           onChange={handleChange} 
