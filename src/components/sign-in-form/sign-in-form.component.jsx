d0e04c09a84bc0aca0ca2047fc3669c707a4b29b
raylin009 <raylin009@gmail.com>
2022-07-01 17:54:18 +0800

    implimented usecontext. can accress user in navigation

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 
 import FormInput from '../form-input/form-input.component'
 import Button from '../button/button.conponent';
+
+import { UserContext } from '../contexts/user.context';
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
@@ -18,6 +20,8 @@ const defaultFormFields = {
 const SignInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
+
+  const { setCurrentUser } = useContext(UserContext)
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
@@ -37,8 +41,11 @@ const SignInForm = () => {
   const handleSubmit = async(event) => {
     event.preventDefault();
     try{
-      const response = await signInAuthUserWithEmailAndPassword(email, password);
-      console.log(response);
+      const { user } = await signInAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      setCurrentUser(user);
       resetFormFields();
     } catch(error){
       switch(error.code) {
