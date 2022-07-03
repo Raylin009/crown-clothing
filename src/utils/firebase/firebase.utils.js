bca0c1a190e228d69de195e78f9bcda6c8523ddb
raylin009 <raylin009@gmail.com>
2022-07-03 20:36:37 +0800

    import data to firebase

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -17,7 +17,8 @@ import {
   doc,
   getDoc,
   setDoc,
-  Firestore,
+  collection,
+  writeBatch,
 } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -43,6 +44,19 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, googleprovider)
 export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleprovider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = collection( db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object)
+  })
+
+  await batch.commit();
+  console.log('done');
+}
 
 export const createUserDocumentFromAuth = async (
   userAuth, 
